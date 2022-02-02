3e399f026d67a3ca03626458d9930e16b8951c3b
coureur12 <80371972+coureur12@users.noreply.github.com>
2022-02-02 05:43:14 +0100

    fourth commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-var questions = document.getElementsByClassName(".text-question");
-var answers = document.getElementsByClassName(".text-answer"); 
-
+const questions = document.getElementsByClassName('text-question');
+const answers = document.getElementsByClassName('text-answer'); 
+    
+for(let i = 0; i < questions.length; i++ ){
+    questions[i].addEventListener("click", function(){
+      if(answers[i].style.display = "none"){
+          answers[i].style.display = "block";
+      }else if (answers[i].style.display = "block"){
+          answers[i].style.display = "none";
+      };
+    })}
