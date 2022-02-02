a8060602e40278c135a306248778afe7708b58f8
coureur12 <80371972+coureur12@users.noreply.github.com>
2022-02-03 00:32:37 +0100

    fifith commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const questions = document.getElementsByClassName('text-question');
 const answers = document.getElementsByClassName('text-answer'); 
-    
+
+
 for(let i = 0; i < questions.length; i++ ){
     questions[i].addEventListener("click", function(){
-      if(answers[i].style.display = "none"){
+      console.log("salut");
+      console.log(i);
+    if(answers[i].style.display == "none"){
+          questions[i].style.fontWeight="bold";
           answers[i].style.display = "block";
-      }else if (answers[i].style.display = "block"){
+      }else{
           answers[i].style.display = "none";
+          questions[i].style.fontWeight= "normal";
       };
     })}
